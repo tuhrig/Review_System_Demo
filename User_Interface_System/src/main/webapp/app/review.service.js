c05b91157970192fd8ab2e708345b7fa0420cff5
Thomas Uhrig <thomas.uhrig@westernacher.com>
2017-11-03 16:31:22 +0100

    Add function to retriev approved review

--- User_Interface_System/src/main/webapp/app/review.service.js
+++ User_Interface_System/src/main/webapp/app/review.service.js
@@ -10,8 +10,15 @@
             return $http.post("reviews", review);
         }
 
+        function getApprovedReviews() {
+            return $http.get("reviews/approved").then(function (result) {
+                return result.data;
+            });
+        }
+
         return {
-            submitReview: submitReview
+            submitReview: submitReview,
+            getApprovedReviews: getApprovedReviews
         };
     }
 }());
--- User_Interface_System/src/test/webapp/app/review.service.spec.js
+++ User_Interface_System/src/test/webapp/app/review.service.spec.js
@@ -3,6 +3,12 @@ describe('ReviewService', function() {
     var ReviewService;
     var $httpBackend;
 
+    var review = {
+        subject: "My Review",
+        rating: 5,
+        content: "This is a review about a great product."
+    };
+
     beforeEach(function() {
 
         module('rsui');
@@ -18,13 +24,17 @@ describe('ReviewService', function() {
     });
 
     it('should post review', function() {
-        var review = {
-            subject: "My Review",
-            rating: 5,
-            content: "This is a review about a great product."
-        };
         $httpBackend.expectPOST('reviews', review).respond(200);
         ReviewService.submitReview(review);
+        $httpBackend.flush();
+    });
+
+    it('should retrieve list of approved reviews', function() {
+        $httpBackend.expectGET('reviews/approved').respond(200, [ review ]);
+        ReviewService.getApprovedReviews().then(function (reviews) {
+            expect(reviews.length).toBe(1);
+            expect(reviews[0]).toEqual(review)
+        });
         $httpBackend.flush();
     });
 });
