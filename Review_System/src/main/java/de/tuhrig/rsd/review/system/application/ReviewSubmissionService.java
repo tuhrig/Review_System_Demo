8262b3babff516a0ed9ada189fd5030d5a8bfba1
Thomas Uhrig <thomas.uhrig@westernacher.com>
2017-11-03 10:41:21 +0100

    Made saving of new reviews transactional

--- Review_System/src/main/java/de/tuhrig/rsd/review/system/application/ReviewSubmissionService.java
+++ Review_System/src/main/java/de/tuhrig/rsd/review/system/application/ReviewSubmissionService.java
@@ -5,6 +5,7 @@ import de.tuhrig.rsd.review.system.domain.ReviewRepository;
 import de.tuhrig.rsd.review.system.ports.event.ReviewSubmittedEventSender;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +14,10 @@ public class ReviewSubmissionService {
     private ReviewRepository reviewRepository;
     private ReviewSubmittedEventSender reviewSubmittedEventSender;
 
+    // This method is transactional as we do two things at the same time:
+    // saving the review and sending a new event. If the event sending fails,
+    // we also want to rollback the saved review.
+    @Transactional
     public void submit(Review review) {
         review.open();
         reviewRepository.save(review);
---
+++ Review_System/src/test/java/de/tuhrig/rsd/review/system/application/ReviewSubmissionServiceTransactionTest.java
@@ -0,0 +1,49 @@
+package de.tuhrig.rsd.review.system.application;
+
+import de.tuhrig.rsd.review.system.domain.Review;
+import de.tuhrig.rsd.review.system.domain.ReviewFixtures;
+import de.tuhrig.rsd.review.system.infrastructure.database.PersistenceConfig;
+import de.tuhrig.rsd.review.system.infrastructure.database.ReviewRepositoryAdapter;
+import de.tuhrig.rsd.review.system.ports.event.ReviewSubmittedEventSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.doThrow;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {
+        PersistenceConfig.class,
+        ReviewRepositoryAdapter.class,
+        ReviewSubmissionService.class
+})
+public class ReviewSubmissionServiceTransactionTest {
+
+    @MockBean
+    private ReviewSubmittedEventSender reviewSubmittedEventSenderMock;
+
+    @Autowired
+    private ReviewRepositoryAdapter reviewRepositoryAdapter;
+
+    @Autowired
+    private ReviewSubmissionService reviewSubmissionService;
+
+    @Test
+    public void should_NotSaveReview_IfEventSendingFails() {
+
+        Review review = ReviewFixtures.anInitialFiveStarSmartphoneReview();
+        doThrow(new RuntimeException()).when(reviewSubmittedEventSenderMock).reviewSubmitted(review);
+
+        try {
+            reviewSubmissionService.submit(review);
+        } catch (RuntimeException e){
+            // we ignore this exception as it's the one we've mocked before...
+        }
+        Review loaded = reviewRepositoryAdapter.find(review.getReviewId());
+        assertThat(loaded).isNull(); // Nothing must be saved as the transaction was rolled back!
+    }
+}
